f9bb51bd477bcca33b995ae022cd9c53f754fff5
Cleonir Silva <82469705+cleosilva@users.noreply.github.com>
2021-07-06 11:35:13 -0300

    recebendo input e retornando por categoria

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const livros = require('./database')
-console.log(livros)
-
 // pegar o input
+// Se for Sim, mostra as categorias disponíveis, pergunta qual categoria ela escolhe
+// Se não, mostra todos os livros em ordem crescente pela quantidade de páginas
 
+const livros = require('./database')
 const readline = require('readline-sync')
+
+const entradaInicial = readline.question('Deseja buscar um livro?S/N')
+
+if (entradaInicial.toLocaleUpperCase() === 'S') {
+    console.log('Essas são as categorias disponíveis:');
+    console.log('Produtividade', '/História brasileira', '/Américas', '/Tecnologia',
+    '/Estilo de vida');
+
+    const entradaCategoria = readline.question('Qual categoria escolhida: ');
+
+    const retorno = livros.filter(livro => livro.categoria === entradaCategoria);
+
+    console.table(retorno);
+}
+
